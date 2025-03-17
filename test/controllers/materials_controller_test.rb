689e9823233f97e96f067b253bd0a54371210655
ShoyaUeno1026 <ueno@hab-co.jp>
2025-03-17 21:48:22 +0900

    materials_controller_test.rbの削除

--- test/controllers/materials_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class MaterialsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get materials_index_url
-    assert_response :success
-  end
-end

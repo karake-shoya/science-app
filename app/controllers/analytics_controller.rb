94d36906835bea7c063058c39c3d9adc802bb872
ShoyaUeno1026 <ueno@hab-co.jp>
2025-08-18 05:24:09 +0900

    営業日数から有給等を減算し、稼働時間計算に反映

--- app/controllers/analytics_controller.rb
+++ app/controllers/analytics_controller.rb
@@ -14,7 +14,7 @@ class AnalyticsController < ApplicationController
     target_date = calculate_until == "yesterday" ? @today.yesterday : @today
     @today_formatted = target_date.strftime("%-m月%d日 (%a)")
 
-    @hours_u1 = @total_duration_hours
+    @hours_u1 = params[:total_hours_user1].present? ? params[:total_hours_user1].to_f : @total_duration_hours
     @hours_u2 = params[:total_hours_user2].to_f || 0.0
 
     start_date = target_date.beginning_of_month
@@ -22,18 +22,20 @@ class AnalyticsController < ApplicationController
 
     Rails.logger.info("Date range: #{start_date} to #{end_date}")
 
-    @business_days = (start_date..target_date).count { |date| business_day?(date) }
-    @remaining_days = (target_date.next_day..end_date).count { |date| business_day?(date) }
+  @business_days = (start_date..target_date).count { |date| business_day?(date) }
+  subtract_days = params[:subtract_days].to_i
+  @business_days_adjusted = [@business_days - subtract_days, 0].max
+  @remaining_days = (target_date.next_day..end_date).count { |date| business_day?(date) }
 
-    @per_day_u1 = @business_days > 0 ? (@hours_u1 / @business_days) : 0
-    @predicted_u1 = @per_day_u1 * @remaining_days
-    @final_u1 = @hours_u1 + @predicted_u1
+  @per_day_u1 = @business_days_adjusted > 0 ? (@hours_u1 / @business_days_adjusted) : 0
+  @predicted_u1 = @per_day_u1 * @remaining_days
+  @final_u1 = @hours_u1 + @predicted_u1
 
-    @per_day_u2 = @business_days > 0 ? (@hours_u2 / @business_days) : 0
-    @predicted_u2 = @per_day_u2 * @remaining_days
-    @final_u2 = @hours_u2 + @predicted_u2
+  @per_day_u2 = @business_days_adjusted > 0 ? (@hours_u2 / @business_days_adjusted) : 0
+  @predicted_u2 = @per_day_u2 * @remaining_days
+  @final_u2 = @hours_u2 + @predicted_u2
 
-    @total_final = @final_u1 + @final_u2
+  @total_final = @final_u1 + @final_u2
   end
 
   private

0acd660d64a307d7d164731daef588de187edee8
ShoyaUeno1026 <ueno@hab-co.jp>
2025-11-12 17:05:19 +0900

    feat: dashboardにqiitaトレンド表示の追加

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,15 @@
+require 'open-uri'
+require 'rss'
+
 class HomeController < ApplicationController
   allow_unauthenticated_access only: [ :index ]
   def index
   end
 
   def dashboard
+    url = 'https://qiita.com/popular-items/feed'
+    rss = URI.open(url).read
+    feed = RSS::Parser.parse(rss, false)
+    @qiita_trends = feed.items.first(15)
   end
 end

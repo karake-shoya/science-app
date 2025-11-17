1c3c0d33d9d47e566ea628a20df8233c369c0b8a
ShoyaUeno1026 <ueno@hab-co.jp>
2025-11-17 15:02:49 +0900

    feat: ユーザー新規登録処理の追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,25 @@
 class UsersController < ApplicationController
+  allow_unauthenticated_access only: [:new, :create]
   def show
     @user = Current.user
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      start_new_session_for @user
+      redirect_to dashboard_path, notice: "User created successfully."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email_address, :password, :password_confirmation)
   end
 end
